bbe63ab6aade8ecfe4e0a53c6453019776841a80
mattwright324 <mattwright324@gmail.com>
2025-07-03 08:11:44 -0400

    update 17.1.1 hotfix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     function init() {
         new ClipboardJS(".clipboard");
 
-        const LATEST_SERVER_VERSION = 2378492222;
+        const LATEST_SERVER_VERSION = 1167549499;
         const gs = {
             serverVersion: {
                 24371034: "v14.?",
@@ -17,6 +17,7 @@
                 1421582404: "v17",
                 3648679879: "v17.0.1",
                 2378492222: "v17.1",
+                1167549499: "v17.1.1",
             },
             mapDecode: {
                 1: "Foy",
